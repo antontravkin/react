d6d7a4c657ee92b6396548041de141b63df4e6c6
Anton Travkin <travkin.rh@gmail.com>
2024-03-02 02:44:52 +0300

    map

--- 01-react-basic-start/src/App.jsx
+++ 01-react-basic-start/src/App.jsx
@@ -1,11 +1,26 @@
 import Header from './components/Header'
 import WebDeveloper from './components/WebDeveloper'
 import Button from './components/Button/Button'
-
-import { WebDevelopers } from './data'
+import { useState } from 'react'
+import { WebDevelopers, differences } from './data'
 
 
 export default function App() {
+    const [contentType, setContentType] = useState(null)
+
+    function handleClick(type) {
+        console.log('click', type)
+        setContentType(type)
+    }
+
+    /* let tabContent = null
+    if (contentType){
+        tabContent = <p>{differences[contentType]}</p>
+    }
+    else{
+        tabContent = <p>Click btn</p>
+    } */
+
     return (
         <div>
             <Header />
@@ -13,11 +28,23 @@ export default function App() {
                 <section>
                     <h3>WEB-РАЗРАБОТЧИК</h3>
                     <ul>
-                        {WebDevelopers.map(el => (
-                            <WebDeveloper title={el.title} description={el.description} />
-                        ))}
+                        {WebDevelopers.map(el =>
+                            <WebDeveloper key={el.title} {...el} />)}
                     </ul>
-                    <Button />
+                    <Button isActive={contentType === 'click1'} onClick={() => handleClick('click1')}>Click 1</Button>
+                    <Button isActive={contentType === 'click2'} onClick={() => handleClick('click2')}>Click 2</Button>
+                    <Button isActive={contentType === 'click3'} onClick={() => handleClick('click3')}>Click 3</Button>
+
+                    {/* {contentType ? <p>{differences[contentType]}</p> : <p>Click btn</p>}
+
+                    {!contentType ? <p>Click btn</p> : null}
+                    {contentType ? <p>{differences[contentType]}</p> : null} */}
+
+                    {!contentType && <p>Click btn</p>}
+                    {contentType && <p>{differences[contentType]}</p>}
+
+                    {/* {tabContent} */}
+
                 </section>
             </main>
         </div>
