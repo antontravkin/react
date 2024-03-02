655e97dd20a76ca7a60ea65051a263f2238a7109
Anton Travkin <travkin.rh@gmail.com>
2024-03-02 14:29:28 +0300

    useRef

--- 01-react-basic-start/src/components/Button/Button.jsx
+++ 01-react-basic-start/src/components/Button/Button.jsx
@@ -1,15 +1,16 @@
 import classes from './Button.module.css'
 
-export default function Button({ children, onClick, isActive }) {
+export default function Button({ children, isActive, ...props }) {
 
 
     /* let classes = 'button'
     if (isActive) classes += ' active' */
     return (
+
         //<button className={isActive ? 'button active' : 'button'} onClick={onClick} onDoubleClick={() => console.log('test')} >{children}</button>
-        <button className={
+        <button {...props} className={
             isActive ? `${classes.button} ${classes.active}` : classes.button
-        } onClick={onClick}>
+        } >
             {children}
         </button >
 
--- 01-react-basic-start/src/components/FeedbackSection.jsx
+++ 01-react-basic-start/src/components/FeedbackSection.jsx
@@ -1,40 +1,80 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import Button from './Button/Button'
 
 
 export default function FeedbackSection() {
-    const [name, setName] = useState()
+
+    function StateVsRef() {
+        const input = useRef()
+        const [show, setShow] = useState(false)
+
+        function handleKeyDown(e) {
+            if (e.key === 'Enter') {
+                setShow(true)
+            }
+        }
+        console.log(input)
+        //debugger отладка
+        return (
+            <div>
+                {/* <h2>{input.current?.value}</h2> */}
+
+                <h3>Input value: {show && input.current.value}</h3>
+                <input ref={input} onKeyDown={handleKeyDown} type="text" />
+            </div>
+
+        )
+    }
+    /* onChange={(e) => setValue(e.target.value)} */
+    /* const [name, setName] = useState()
     const [hasError, setHasError] = useState(false)
-    const [reason, setReason] = useState('help')
+    const [reason, setReason] = useState('help') */
+
+    const [form, setForm] = useState({
+        name: '',
+        hasError: false,
+        reason: 'help'
+    })
 
     function heandelChangeName(event) {
-
-        setName(event.target.value)
-        setHasError(event.target.value.trim().length === 0)
+        setForm((pref) => ({
+            ...pref,
+            name: event.target.value,
+            hasError: event.target.value.trim().length === 0,
+        }))
     }
+
+    /* function toggleError() {
+        setForm({
+            hasError: (prev) => !prev
+        })
+        setForm.hasError((prev) => !prev) меняет текущее состояние
+    } */
 
     return (
         <section>
             <h3>Обратная связь</h3>
-
+            {/* <Button onClick={toggleError}> toggleError</Button> */}
             <form action="">
-                <label htmlFor="namr">Имя</label>
-                <input className="control" id="name" type="text" placeholder="напишите имя" value={name}
-                    style={{ border: hasError ? '1px solid red' : null }} onChange={heandelChangeName} />
+                <label htmlFor="name">Имя</label>
+                <input className="control" id="name" type="text" placeholder="напишите имя" value={form.name}
+                    style={{ border: form.hasError ? '1px solid red' : null }} onChange={heandelChangeName} />
 
                 <label htmlFor="reason">Причина обращения</label>
-                <select id="reason" className="control" value={reason} onChange={(event) => setReason(event.target.value)}>
+                <select id="reason" className="control" value={form.reason} onChange={(event) => setForm((pref) => ({ ...pref, reason: event.target.value }))}>
                     <option value="error">Ошибка</option>
                     <option value="help">Помощь</option>
                     <option value="suggest">Предложение</option>
                 </select>
 
-                <Button>Отправить</Button>
+                <Button style={{ marginBottom: '1rem' }} disabled={form.hasError} isActive={!form.hasError}>Отправить</Button>
             </form>
             <pre>
-                name: {name}
-                reason: {reason}
+                name: {form.name}
+                reason: {form.reason}
             </pre>
+            <hr />
+            <StateVsRef />
         </section>
 
     )
